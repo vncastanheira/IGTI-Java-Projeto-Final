b79cbc53a118dc808cefcf5bf10cc7d90820a769
Vinicius <Vinicius@VAPOR>
2020-11-14 15:03:19 -0300

    Init

---
+++ app/src/main/java/com/vinicius/igti_java_projetofinal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vinicius.igti_java_projetofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
